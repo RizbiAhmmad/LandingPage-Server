4164ffbff0a1e4760f58c9d0f764318f5b95df8c
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-09-07 12:34:34 +0600

    Storing and getting user information and order information from backend

---
+++ index.js
@@ -0,0 +1,153 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jwqfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+const database = client.db("Landing-Page");
+    const usersCollection = database.collection("users");
+    const productsCollection = database.collection("products");
+    const ordersCollection = database.collection("orders");
+
+     // POST endpoint to save user data (with role)
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      // console.log(req.headers);
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Get role by email
+    app.get("/users/role", async (req, res) => {
+      const email = req.query.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      if (!user) {
+        return res.status(404).send({ role: null, message: "User not found" });
+      }
+      res.send({ role: user.role });
+    });
+
+    // PATCH endpoint to make a user an admin by ID
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // DELETE endpoint to remove a user
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+// Add product
+app.post("/products", async (req, res) => {
+  const product = req.body;
+  const result = await productsCollection.insertOne(product);
+  res.send(result);
+});
+
+// Get all products
+app.get("/products", async (req, res) => {
+  const result = await productsCollection.find().toArray();
+  res.send(result);
+});
+
+// Get single product by id
+app.get("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productsCollection.findOne(query);
+  res.send(result);
+});
+
+// Delete product
+app.delete("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productsCollection.deleteOne(query);
+  res.send(result);
+});
+
+ app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
+
+    // Get all orders
+    app.get("/orders", async (req, res) => {
+      const result = await ordersCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to you in Landing page');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
